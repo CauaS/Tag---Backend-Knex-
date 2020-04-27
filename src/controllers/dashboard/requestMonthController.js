6d0290a5a439b4e118391c1afe9d94ea7ed88756
Caligiuri Cauã dos Santos <kalikaua@gmail.com>
2020-04-26 23:04:30 -0300

    Change
    Adicionado um group by e count ao select

--- src/controllers/dashboard/requestMonthController.js
+++ src/controllers/dashboard/requestMonthController.js
@@ -5,14 +5,14 @@ module.exports = {
         try{
             const response = await connection('request')
             .select(                
-                'ty.description as type',
-                'r.number'             
+                'ty.description as type',       
             )
+            .count('r.number as qtd')
             .from('request as r')
             .join('type as ty', 'ty.id','=', 'r.type_id')
             .join('consultant as c', 'c.id','=', 'r.consultant_id')
             .whereBetween('created_at', ['2020-04-27 00:00:00', '2020-04-27 06:59:27'])
-                
+            .groupBy('ty.description') 
                 
                 return resp.json(response);
         }catch(erro){
