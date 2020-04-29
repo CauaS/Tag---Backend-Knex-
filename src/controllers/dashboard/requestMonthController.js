9826dd4b1069f2add9ada4e1239943b559946406
Caligiuri Cauã dos Santos <kalikaua@gmail.com>
2020-04-29 00:24:44 -0300

    Change
    Alterada a forma  de busca para data mais recente no mês.

--- src/controllers/dashboard/requestMonthController.js
+++ src/controllers/dashboard/requestMonthController.js
@@ -2,6 +2,11 @@ const connection = require('../../database/connections');
 
 module.exports = {
     async show(req, resp) {
+        const date = new Date();
+        const DAY = date.getDate() > 10 ? `0${date.getDate()}`: date.getDate ;
+        const MONTH = date.getMonth()+1 > 10 ? `0${date.getMonth()+1}` : date.getMonth()+1;
+        const YEAR = date.getFullYear();
+
         try{
             const response = await connection('request')
             .select(                
@@ -11,7 +16,7 @@ module.exports = {
             .from('request as r')
             .join('type as ty', 'ty.id','=', 'r.type_id')
             .join('consultant as c', 'c.id','=', 'r.consultant_id')
-            .whereBetween('created_at', ['2020-04-27 00:00:00', '2020-04-27 06:59:27'])
+            .whereBetween('created_at', ['2020-04-27 00:00:00', `${YEAR}-${MONTH}-${DAY} 00:00:00`])
             .groupBy('ty.description') 
                 
                 return resp.json(response);
