f5ac9bc1f14a86d8010268257be725035290bac9
Caligiuri Cauã dos Santos <kalikaua@gmail.com>
2020-05-04 22:21:56 -0300

    Change
    Ajustado a formatação de data.

--- src/controllers/dashboard/requestMonthController.js
+++ src/controllers/dashboard/requestMonthController.js
@@ -4,8 +4,12 @@ module.exports = {
     async show(req, resp) {
         const date = new Date();
         const DAY = date.getDate() > 10 ? `0${date.getDate()}`: date.getDate() ;
-        const MONTH = date.getMonth()+1 > 10 ? `0${date.getMonth()+1}` : date.getMonth()+1;
+        const MONTH = date.getMonth()+1 < 10 ? `0${date.getMonth()+1}` : date.getMonth()+1;
+        const PREV_MONTH = date.getMonth() < 10 ? `0${date.getMonth()}` : date.getMonth();
         const YEAR = date.getFullYear();
+
+        console.log(PREV_MONTH);
+        console.log(MONTH);
 
         try{
             const response = await connection('request')
@@ -16,7 +20,7 @@ module.exports = {
             .from('request as r')
             .join('type as ty', 'ty.id','=', 'r.type_id')
             .join('consultant as c', 'c.id','=', 'r.consultant_id')
-            .whereBetween('created_at', [`${YEAR}-${MONTH-1}-01 00:00:00`, `${YEAR}-${MONTH}-${DAY} 00:00:00`])
+            .whereBetween('created_at', [`${YEAR}-${PREV_MONTH}-01 00:00:00`, `${YEAR}-${MONTH}-${DAY} 00:00:00`])
             .groupBy('ty.description') 
                 
                 return resp.json(response);
