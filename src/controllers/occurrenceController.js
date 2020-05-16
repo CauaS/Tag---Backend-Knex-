82b1ed8a3b47edd086934e58a11be4b759e9dd8e
Caligiuri Cauã dos Santos <kalikaua@gmail.com>
2020-05-16 20:43:07 -0300

    New
    Adicionada função para retornar as ocorrencias de um pedido específico.

--- src/controllers/occurrenceController.js
+++ src/controllers/occurrenceController.js
@@ -43,5 +43,33 @@ module.exports = {
         }catch(erro){
             return resp.json({ message: `Something when wrong: ${erro}`})
         }
+    },
+    async index(req, resp){
+
+        const { number } = req.body;
+        
+        try{
+            const response = await connection('occurrence')
+                .select(
+                     'r.number',
+                     'r.created_at as request_date',
+                     'ocor.comment',
+                     'e.description as event_description',
+                     'ocor.created_at as ocor_date',
+                     'c.description as consultant',
+                     'ty.description as type', 
+                     'ty.color', 
+                    )
+                .from('occurrence as ocor')
+                .where('r.number', number)
+                .join('request as r', 'r.id', '=', 'ocor.request_id')
+                .join('event as e'  , 'e.id', '=', 'ocor.event_id')
+                .join('type as ty', 'ty.id','=', 'r.type_id')
+                .join('consultant as c', 'c.id','=', 'r.consultant_id');
+
+            return resp.json(response);
+        }catch(erro){
+            return resp.json({ message: `Something when wrong: ${erro}`})
+        }
     }
 }
