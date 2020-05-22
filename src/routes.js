d8067afe0d5616c3e03e209892ac50f95fab7c93
Caligiuri Cauã dos Santos <kalikaua@gmail.com>
2020-05-21 23:17:11 -0300

    Change
    Adicionadas novas rotas.

--- src/routes.js
+++ src/routes.js
@@ -5,11 +5,20 @@ const  consultController = require('./controllers/consultController');
 const  typeController = require('./controllers/typeController');
 const  requestController = require('./controllers/requestController');
 const  tagsController = require('./controllers/tagsController');
+
 const  requestMonthController = require('./controllers/dashboard/requestMonthController');
 const  requestPatchController = require('./controllers/dashboard/requestPatchController');
+
 const  eventController = require('./controllers/eventController');
 const  occurrenceController = require('./controllers/occurrenceController');
-const  scrumController = require('./controllers/scrumController');
+
+const  scrumController = require('./controllers/Scrum/scrumController');
+const  scrumLiberadoController = require('./controllers/Scrum/scrumLiberadoController');
+const  scrumAnalisadoController = require('./controllers/Scrum/scrumAnalisadoController');
+const  scrumValidadoDesenvController = require('./controllers/Scrum/scrumValidadoDesenvController');
+const  scrumValidadoConsultController = require('./controllers/Scrum/scrumValidadoConsultController');
+const  scrumAtualizadoClienteController = require('./controllers/Scrum/scrumAtualizadoClienteController');
+const  scrumImpedidoController = require('./controllers/Scrum/scrumImpedidoController');
 
 
 routers.post('/consultant', consultController.create);
@@ -43,5 +52,11 @@ routers.post('/occurrence', occurrenceController.create);
 routers.post('/occurrence/request', occurrenceController.index);
 
 routers.get('/scrum', scrumController.show);
+routers.get('/scrum/liberado', scrumLiberadoController.show);
+routers.get('/scrum/analisado', scrumAnalisadoController.show);
+routers.get('/scrum/validadoDesenv', scrumValidadoDesenvController.show);
+routers.get('/scrum/validadoConsult', scrumValidadoConsultController.show);
+routers.get('/scrum/atualizadoCliente', scrumAtualizadoClienteController.show);
+routers.get('/scrum/impedido', scrumImpedidoController.show);
 
 module.exports = routers;
