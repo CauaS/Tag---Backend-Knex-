6cd9e2cfaa0c07a7bdcbf3a563f8dee719b98b36
Caligiuri Cauã dos Santos <kalikaua@gmail.com>
2020-05-21 23:15:35 -0300

    New
    Criadas rotas individuais para cada coluna so scrum.

---
+++ src/controllers/Scrum/scrumAtualizadoClienteController.js
@@ -0,0 +1,41 @@
+const connection = require('../../database/connections');
+const knex = require('knex');
+
+module.exports = {
+    async show(req, resp){
+        try{
+            const response = await connection('occurrence')
+                .select(
+                     'r.number',
+                     knex.raw(`strftime('%d${'/'}%m${'/'}%Y', date(r.created_at)) as request_date`),
+                     'ocor.comment',
+                     'e.description as event_description',
+                     'e.id as event_id',
+                     knex.raw(`strftime('%d${'/'}%m${'/'}%Y', date(ocor.created_at)) as ocor_date`),
+                     'c.description as consultant',
+                     'ty.description as type', 
+                     'ty.color', 
+                    )
+                .from('occurrence as ocor')
+                .where('e.description', '=', 'Atualizado Cliente')
+                .join('request as r', 'r.id', '=', 'ocor.request_id')
+                .join('event as e'  , 'e.id', '=', 'ocor.event_id')
+                .join('type as ty', 'ty.id','=', 'r.type_id')
+                .join('consultant as c', 'c.id','=', 'r.consultant_id');
+
+
+            //if there is a property event_description with value 'Analisado', so is added the property 'analisado' to the item itself.
+            const fResponse = response.map(item => { 
+                if(item.event_description.includes('Analisado')){
+                   return Object.assign({}, item, { analisado: true });
+                }else{
+                    return Object.assign({}, item, { analisado: false });
+                }
+            });
+
+            return resp.json(fResponse);
+        }catch(erro){
+            return resp.json({ message: `Something when wrong: ${erro}`})
+        }
+    },
+}
